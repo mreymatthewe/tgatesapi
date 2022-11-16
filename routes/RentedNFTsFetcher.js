11d09ef3a23344ad3e1433f1d6b2615b9ad86804
mreymatthewe <mreymatthew@gmail.com>
2022-11-16 18:16:47 +0800

    parse Int

--- routes/RentedNFTsFetcher.js
+++ routes/RentedNFTsFetcher.js
@@ -6,7 +6,11 @@ const sdk = new ThirdwebSDK("goerli");
 const contract = await sdk.getContract(Address());
 
 router.get("/", async (req, res) => {
-  res.send("Please Enter the Wallet Address");
+  const data = await contract.call(
+    "getEscrowsByAddress",
+    "0xfD72C8923EF3236aFf248aAAecdA956e78824a95"
+  );
+  res.send(data);
 });
 
 router.post("/", async (req, res) => {
@@ -15,19 +19,30 @@ router.post("/", async (req, res) => {
   let data = {};
   let tokenIds = [];
   let filteredTokenIds = [];
+  let arrayTokenIds = [];
   try {
     data = await contract.call("getEscrowsByAddress", addresses.walletaddress);
     tokenIds = Object.entries(data);
-    for (let x = 0; x <= tokenIds.length; x++) {
-      if (tokenIds[x][1][2] == addresses.contractaddress) {
+
+    for (let x = 0; x < tokenIds.length; x++) {
+      if (addresses.contractaddress == tokenIds[x][1][2]) {
         filteredTokenIds.push(tokenIds[x][1][1]);
+
+        arrayTokenIds.push(
+          parseInt(Object.values(filteredTokenIds[x])[0].slice(3))
+        );
       }
     }
   } catch {
     tokenIds = "No Results. Address you entered may have no records or invalid";
   }
-
-  res.send(filteredTokenIds);
+  arrayTokenIds.push(
+    parseInt(
+      Object.values(filteredTokenIds[filteredTokenIds.length - 1])[0].slice(3)
+    )
+  );
+  console.log(arrayTokenIds);
+  res.send(arrayTokenIds);
 });
 
 export default router;
